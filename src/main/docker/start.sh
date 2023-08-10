0783ec173820b5457347d2f765f487821f4ed649
zhouxiaoze <111@qq.com>
2023-08-10 16:16:52 +0800

    增加docker compose对容器进行编排

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -10,8 +10,9 @@ mkdir -p /home/data/volumes/redis/redis.conf
 chmod 777 /home/data/volumes/redis/redis.conf
 echo '创建redis数据卷完成'
 echo '开始创建compose服务镜像'
-docker build -f Dockerfile -t compose-demo
+docker build -f /home/compose/Dockerfile -t /home/compose-demo/
 echo '构建compose镜像成功'
 echo '开始创建容器'
-docker-compose up -d
+docker compose up -d
 echo '创建容器成功'
+
