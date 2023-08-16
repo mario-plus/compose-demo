fab51299a5b13b38fc9b1431acfa3ef582616d14
zhouxiaoze <111@qq.com>
2023-08-16 10:28:53 +0800

    优化脚本

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
+#创建文件夹
+if [ ! -d "/home/compose" ]; then
+mkdir -p /home/compose
+else
+rm -rf /home/compose
+mkdir /home/compose
+chmod 777 /home/compose
+fi
+
+if [ ! -d "/home/data/volumes/mysql" ]; then
 mkdir -p /home/data/volumes/mysql/conf
 chmod 777 /home/data/volumes/mysql/conf
 mkdir -p /home/data/volumes/mysql/data
 chmod 777 /home/data/volumes/mysql/data
 echo '创建mysql数据卷完成'
+else
+echo 'mysql数据卷存在，使用原有数据卷创建容器'
+fi
+
+if [ ! -d "/home/data/volumes/redis" ]; then
 mkdir -p /home/data/volumes/redis/data
 chmod 777 /home/data/volumes/redis/data
 mkdir -p /home/data/volumes/redis/redis.conf
 chmod 777 /home/data/volumes/redis/redis.conf
 echo '创建redis数据卷完成'
+else
+echo 'Redis数据卷存在，使用原有数据卷创建容器'
+fi
+
 echo '开始创建compose服务镜像'
 docker build -f /home/compose/Dockerfile -t compose-demo /home/compose/
 echo '构建compose镜像成功'
