f241fcc2a7d4b15eac8616cb446f2234ff4c4278
zhouxiaoze <111@qq.com>
2023-08-29 18:11:43 +0800

    gateway调试

--- cloud-alibaba/gateway/gateway-cli/src/main/java/com/mario/GatewayCliApplication.java
+++ cloud-alibaba/gateway/gateway-cli/src/main/java/com/mario/GatewayCliApplication.java
@@ -2,11 +2,13 @@ package com.mario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author zxz
- * @date 2023年08月29日 15:50
+ * @date 2023年08月29日 17:53
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayCliApplication {
     public static void main(String[] args) {
