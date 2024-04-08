b3dfda629aa8c9154e3c1d2847fcf449e8c92901
zhouxiaoze <111@qq.com>
2024-04-08 20:06:47 +0800

    测试zookeeper集群

--- component/zookeeperS/src/main/java/com/mario/ZookeeperApplication.java
+++ component/zookeeperS/src/main/java/com/mario/ZookeeperApplication.java
@@ -1,18 +1,33 @@
 package com.mario;
 
+import com.mario.service.impl.ZkServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author zxz
  * @date 2023年08月18日 9:47
  */
 @SpringBootApplication
-public class ZookeeperApplication{
+public class ZookeeperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZookeeperApplication.class, args);
     }
 
+    @Autowired
+    ZkServiceImpl zkService;
 
+    @PostConstruct
+    public void init() {
+        try {
+            String data = zkService.getData("/controller");
+            System.out.println(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
